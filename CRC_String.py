a910bdb4a208483dc747ec3ab2c454cdfb085111
co2plant <jaonz6057@wku.ac.kr>
2022-12-15 20:17:08 +0900

    보기 편하도록 변경 + Numpy Array[Boolean]형태로 변환해서 처리 + 판단 이후에 콘솔에 출력해서 보여줌 + 저장은 되어있지 않음.

--- CRC_String.py
+++ CRC_String.py
@@ -1,99 +1,39 @@
 import sys
 import numpy as np
-from scapy.all import *
 
-# ethernet maxLength = 1518Byte
-# ethernet minLength = 64Byte
-# ethernet CRCLength = 4Byte
+class CRC_String():
 
-# ethernet packet
-# preamble = 7Byte + 1Byte(SFD) <- Layer1
-# Destination Address = 6Byte
-# Source Address = 6Byte
-# Length and Type = 2Byte
-# Data and Padding = 46Byte ~ 1500Byte
-# CRC = 4Byte
+    def __init__(self):
+        print("Start")
 
-class CRC_String:
-    # Binary Code of Divisor, Packet, Initial, Temp, Result
-    global DivisorCode
-    global PacketCode
-    global initialCode
-    tempCode=0b1
-    global result
+    #input type Integer
+    #return type numpy array String
+    def changeToStr(inputCode):
+        result = str(bin(inputCode))[2:]
+        result = np.array(list(result))
+        print(result)
+        print(result.size)
+        return result
 
-    # Length of Divisor, Packet, Initial, Temp
-    DivisorLength = 0
-    PacketLength = 0
-    initialLength = 0
-    tempLength = 1
+    #input type String
+    #return type Integer
+    def countCodeSize(inputCode):
+        result = inputCode.size
+        print(result)
+        return result
 
-    def __init__(self, Divisor, Packet, initial):
-        self.DivisorCode = np.array(list(bin(Divisor)))
-        #self.PacketCode = np.array(list(str(bin(int(Packet)))))
-        #self.initialCode = np.array(list(str(bin(int(initial)))))
+    #input type numpy array input
+    #return type numpy array boolean
+    def changeToBool(inputCode):
+        result = inputCode.astype('bool')
+        print(result)
+        return result
 
-        self.DivisorLength = self.DivisorCode.size
-        self.PacketLength = self.PacketCode.size + self.DivisorLength
-        self.initialLength = self.initialCode.size # initialLength = CRCLength
-        self.tempLength = 1
+    #this function is simple code for main
+    def changeTotal(self, inputCode):
+        result = str(bin(inputCode))[2:]
+        result = np.array(list(result))
+        resultsize = result.size
+        result = result.astype('bool')
 
-        print(self.DivisorCode)
-        print(self.PacketCode)
-        print(self.initialCode)
-
-    def GetCRC(self):
-        tmpPacketCode = (self.PacketCode << self.initialLength) + self.initialCode
-        print("PacketCode  : " + bin(self.PacketCode))
-        tmpDivisorCode = self.DivisorCode << (self.PacketLength - self.DivisorLength)
-        print("DivisorCode : " + bin(self.DivisorCode))
-        tmptempCode = self.tempCode << (self.PacketLength - self.tempLength)
-        print("temp Code   : " + bin(self.tempCode)) 
-        input()
-
-        for i in range(0, self.PacketLength-self.DivisorLength):    
-            if(tmpPacketCode & tmptempCode == tmptempCode):
-                tmpPacketCode = tmpPacketCode ^ tmpDivisorCode
-                print("--taken--")
-            tmpDivisorCode = tmpDivisorCode >> 1
-            tmptempCode = tmptempCode >> 1
-
-            print("-----------------------------------------------")    
-            print(str(i+1) + "번째 패킷: " + str(bin(tmpPacketCode)))
-            print(str(i+1) + "번째 제수: " + str(bin(tmpDivisorCode)))
-            print(str(i+1) + "번째 템프: " + str(bin(tmptempCode)))
-            print("-----------------------------------------------")
-            print("")
-
-        self.result = tmpPacketCode
-
-    def compareCRC(self):
-        tmpPacketCode = (self.PacketCode << self.initialLength) + self.result
-        print("PacketCode  : " + bin(self.PacketCode))
-        tmpDivisorCode = self.DivisorCode << (self.PacketLength - self.DivisorLength)
-        print("DivisorCode : " + bin(self.DivisorCode))
-        tmptempCode = self.tempCode << (self.PacketLength - self.tempLength)
-        print("temp Code   : " + bin(self.tempCode)) 
-
-        for i in range(0, self.PacketLength-self.DivisorLength):    
-            if(tmpPacketCode & tmptempCode == tmptempCode):
-                tmpPacketCode = tmpPacketCode ^ tmpDivisorCode
-                print("----------------------taken--------------------")
-            tmpDivisorCode = tmpDivisorCode >> 1
-            tmptempCode = tmptempCode >> 1
-
-            print("-----------------------------------------------")    
-            print(str(i+1) + "번째 패킷: " + str(bin(tmpPacketCode)))
-            print(str(i+1) + "번째 제수: " + str(bin(tmpDivisorCode)))
-            print(str(i+1) + "번째 템프: " + str(bin(tmptempCode)))
-            print("-----------------------------------------------")
-            print("")
-
-        print(bin(tmpPacketCode))
-        if(bin(tmpPacketCode) == bin(self.initialCode)):
-            print("This is valiable packet!")
-        else:
-            print("Tihs is wrong packet!")
-
-    def crackCRC(self):
-        self.result = self.result-1
+        return result, resultsize
--- main_String.py
+++ main_String.py
@@ -1,18 +1,60 @@
-import CRC_String
+import numpy as np
+from CRC_String import CRC_String
+
+# ethernet maxLength = 1518Byte
+# ethernet minLength = 64Byte
+# ethernet CRCLength = 4Byte
+
+# ethernet packet
+# preamble = 7Byte + 1Byte(SFD) <- Layer1
+# Destination Address = 6Byte
+# Source Address = 6Byte
+# Length and Type = 2Byte
+# Data and Padding = 46Byte ~ 1500Byte
+# CRC = 4Byte
+ 
+# Binary Code of Divisor, Packet, Initial, Temp, Result
 
 #----------------------------Initialize Field
-divisorCode = 0x04C11DB7
-packetData = 0x705dcce4a1c800e04c362a9f08004500002cb26200004001ebb8c0a81a0c0101010108007e7264b7000012cfb5deee1d4d72821135d058700046
-initialCode = 0xFFFFFFF
+DivisorCode=0x04C11DB7
+PacketCode=0x705dcce4a1c800e04c362a9f08004500002cb26200004001ebb8c0a81a0c0101010108007e7264b7000012cfb5deee1d4d72821135d058700046
+initialCode=0xFFFFFFFF
 #--------------------------------------------
 
+modules = CRC_String()
 
-# Binary Code of Divisor, Packet, Initial
-modulesCRC = CRC_String.CRC_String(divisorCode, packetData, initialCode)
+tmpPacket, packetSize = modules.changeTotal(PacketCode)
+tmpDivisor, divisorSize = modules.changeTotal(DivisorCode)
+tmpInitial, initialSize = modules.changeTotal(initialCode)
 
+inputPacket = np.append(tmpPacket, tmpInitial, axis=0)
 
-#modulesCRC.GetCRC()
-#select = input("if you want crack packet input crack")
-#if(select == 'crack'):
-#    modulesCRC.crackCRC()
-#modulesCRC.compareCRC()
+for i in range(0, packetSize):
+    if(inputPacket[i]==True):
+        result = np.logical_xor(inputPacket[i:(i+divisorSize)], tmpDivisor)
+        inputPacket[i:(i+divisorSize)] = result
+        print(inputPacket)
+    #print(str(i+1) + "---while---")
+
+result = inputPacket[packetSize:]
+print("-----")
+print(inputPacket[packetSize:])
+print("-----")
+
+inputPacket = np.append(tmpPacket, result, axis=0)
+
+for i in range(0, packetSize):
+    if(inputPacket[i]==True):
+        result = np.logical_xor(inputPacket[i:(i+divisorSize)], tmpDivisor)
+        inputPacket[i:(i+divisorSize)] = result
+        print(inputPacket)
+    #print(str(i+1) + "---while---")
+
+print(inputPacket[packetSize:])
+result = inputPacket[packetSize:]
+print(tmpInitial)
+
+if(np.array_equal(result, tmpInitial)):
+    print("Is it correct!")
+else:
+    print("retry")
